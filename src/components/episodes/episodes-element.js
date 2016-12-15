be51e295211f8c5d89eee6567173920a99ab994c
Muhammad Moaz <Moaz@Muhammads-MacBook-Pro.local>
2016-12-15 12:43:00 +0300

    Fix tasaamem video loading time

--- src/components/episodes/episodes-element.js
+++ src/components/episodes/episodes-element.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+import YoutubeOnHover from '../../assets/images/youtube/youtube-onhover.svg';
+import Youtube from '../../assets/images/youtube/youtube.svg';
 
 class EpisodesElement extends Component {
+    constructor() {
+        super();
+        this.state = {
+            overlay: `${Youtube}`,
+        }
+    }
     render() {
         const imageURL = this.props.snippet.thumbnails.standard.url;
-        const videoURL = `https://www.youtube.com/embed/${this.props.snippet.resourceId.videoId}`
-        const text = this.props.snippet.title;
+        const videoLink = `https://www.youtube.com/watch?v=${this.props.snippet.resourceId.videoId}`;
+        let text = this.props.snippet.description.substring(0, 200);
+
+        if (text.length !== 0) {
+            text += "..."
+        }
 
         const contentContainer = {
             width: '320px',
@@ -24,25 +36,34 @@ class EpisodesElement extends Component {
         };
 
         const textStyle = {
-            textAlign: 'center'
+            textAlign: 'right'
         };
 
         const embed = {
             backgroundImage: 'url(' + imageURL + ')',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center center',
+            backgroundSize: 'contain',
+            backgroundPosition: 'center center'
         }
 
         return (
             <div className="col-lg-4" style={contentContainer}>
                 <div className="embed-responsive embed-responsive-4by3" style={embed}>
-                    <iframe className="embed-responsive-item" src={videoURL} allowFullScreen/>
+                    {/* <iframe className="embed-responsive-item" src={videoURL} allowFullScreen/> */}
+                    <a href={videoLink} target="blank"><img className="embed-responsive-item" src={this.state.overlay} onMouseOver={this.onMouseOver.bind(this)} onMouseOut={this.onMouseOut.bind(this)} role="presentation"/></a>
                 </div>
                 <div dir="rtl" style={textContainerStyle}>
                     <p style={textStyle}>{text}</p>
                 </div>
             </div>
         );
+    }
+
+    onMouseOver() {
+        this.setState({overlay: `${YoutubeOnHover}`});
+    }
+
+    onMouseOut() {
+        this.setState({overlay: `${Youtube}`});
     }
 };
 
