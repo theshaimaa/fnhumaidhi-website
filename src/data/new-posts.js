12a8917fbbfed3fc50b3f17510ff70af4928da03
Hugo Rodrigues <hrodrigues1984@hotmail.com>
2017-06-14 11:36:47 +0100

    Blog post entry 201706140920 - fix latest posts

--- src/data/new-posts.js
+++ src/data/new-posts.js
@@ -5,15 +5,15 @@ const NewPosts = [
         'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201706140920%2F0.png?alt=media&token=b1eade46-9b1f-440c-ba55-170a063a1495',
     },
     {
-        'title': 'غرفة طفل مودرن بالوان حديثة',
-        'link': 'shopping',
-        'imageURL': "https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/shopping%2Ffive%2Fmain.jpg?alt=media&token=aedeb802-92b2-4480-b225-6a0ee12d8d0d",
+        'title': 'نصائح لكل عروس لبيت الاحلام ولا في الخيال',
+        'link': 'blog-post/201706081440',
+        'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201706081440%2F0.jpg?alt=media&token=0dd96ca7-743a-4526-b12c-acdb1a3ba1fd',
     },
     {
-        'title': 'ابتكارات و افكار للمناشف و الفوط في الحمامات',
-        'link': 'blog-post/5817543854974df59f48d0b51248e165',
-        'imageURL': "https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F5817543854974df59f48d0b51248e165%2F0.jpg?alt=media&token=8bb41dc1-45a1-4a38-8b0f-35f34c9f7c93",
-    },
+        'title': 'لو ارجع ١٣ سنه',
+        'link': 'blog-post/d63b61aa79944aec98a76d1a2d29f001',
+        'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2Fd63b61aa79944aec98a76d1a2d29f001%2F0.jpeg?alt=media&token=c7085c02-cc32-437b-8465-76114caa297d'
+    }
 ];
 
 export default NewPosts;
