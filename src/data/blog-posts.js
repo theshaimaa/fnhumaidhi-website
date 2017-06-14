273dacde90ef5d4126684ef7d09a79fc5fc362b2
Hugo Rodrigues <hrodrigues1984@hotmail.com>
2017-06-14 10:25:06 +0100

    Blog post entry 201706140920

--- src/data/blog-posts.js
+++ src/data/blog-posts.js
@@ -1,5 +1,10 @@
 const BlogPosts = [
     {
+        'title': 'مود بورد الوان باهته لكن جميله',
+        'id': '201706140920',
+        'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201706140920%2F0.png?alt=media&token=b1eade46-9b1f-440c-ba55-170a063a1495',
+    },
+    {
         'title': 'نصائح لكل عروس لبيت الاحلام ولا في الخيال',
         'id': '201706081440',
         'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201706081440%2F0.jpg?alt=media&token=0dd96ca7-743a-4526-b12c-acdb1a3ba1fd',
--- src/data/new-posts.js
+++ src/data/new-posts.js
@@ -1,8 +1,8 @@
 const NewPosts = [
     {
-        'title': 'نصائح لكل عروس لبيت الاحلام ولا في الخيال',
-        'link': 'blog-post/201706081440',
-        'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201706081440%2F0.jpg?alt=media&token=0dd96ca7-743a-4526-b12c-acdb1a3ba1fd',
+        'title': 'مود بورد الوان باهته لكن جميله',
+        'link': 'blog-post/201706140920',
+        'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201706140920%2F0.png?alt=media&token=b1eade46-9b1f-440c-ba55-170a063a1495',
     },
     {
         'title': 'غرفة طفل مودرن بالوان حديثة',
