dc8ef3459d2765eaff94be7304bfa82e0460c4e1
Hugo Rodrigues <hugo.rodrigues@codedesign.org>
2017-08-16 16:14:58 +0100

    Shopping mood board 201708161528

--- src/data/new-posts.js
+++ src/data/new-posts.js
@@ -1,23 +1,23 @@
 const NewPosts = [
     {
+        'title': '‫مدخل بيت مودرن!',
+        'link': 'shopping',
+        'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/shopping%2Fnine%2FTHUMBNAIL_AUGUST_16.jpg?alt=media&token=815452d8-46a9-4b8c-86b8-73bcc13bc57e',
+    },
+    {
         'title': '‫تطبيقات احبها',
         'link': 'blog-post/201708141205',
         'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201708141205%2FTHUMBNAIL_AUGUST_14.jpg?alt=media&token=a2d96a0d-d4c9-4b33-9820-2ee94d4ed988',
     },
-     {
+    {
         'title': 'حديقتي',
         'link': 'blog-post/201708091825',
         'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201708091825%2FTHUMBNAIL_AUGUST_9.jpg?alt=media&token=02d093ba-bc81-44d7-bd90-5c1b0948e6dd',
     },
-     {
-        'title': 'أول مشاريعي',
-        'link': 'blog-post/201708071826',
-        'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201708071826%2FTHUMBNAIL_AUGUST_7.jpg?alt=media&token=a940e2e6-5001-4b06-99e6-e2e2295f1e63',
-    },
-    //  {
-    //     'title': 'غرفة جلوس بستايل المودرن الاستوائي',
-    //     'link': 'shopping',
-    //     'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/shopping%2Feight%2FPhoto%2011-9-16%2C%2011%2012%2050%20AM.jpg?alt=media&token=e64e34f3-e40d-457a-948e-012f005d717d',
+    // {
+    //     'title': 'أول مشاريعي',
+    //     'link': 'blog-post/201708071826',
+    //     'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201708071826%2FTHUMBNAIL_AUGUST_7.jpg?alt=media&token=a940e2e6-5001-4b06-99e6-e2e2295f1e63',
     // },
 ];
 
