24e65eae01b28261fc4bbe10b7724300ef2a942a
Hugo Rodrigues <hugo.rodrigues@codedesign.org>
2017-08-21 18:35:43 +0100

    Video Blog post entry 201708211647

--- src/data/blog-posts.js
+++ src/data/blog-posts.js
@@ -1,5 +1,10 @@
 const BlogPosts = [
     {
+        'title': 'خلط الالوان في الديكور',
+        'id': '201708211647',
+        'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201708211647%2FTHUMBNAIL_AUGUST_21.jpg?alt=media&token=aedc69a2-14ed-4632-bf6a-fe05231fcc34',
+    },
+    {
         'title': 'تطبيقات احبها',
         'id': '201708141205',
         'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201708141205%2FTHUMBNAIL_AUGUST_14.jpg?alt=media&token=a2d96a0d-d4c9-4b33-9820-2ee94d4ed988',
--- src/data/new-posts.js
+++ src/data/new-posts.js
@@ -1,5 +1,10 @@
 const NewPosts = [
     {
+        'title': 'خلط الالوان في الديكور',
+        'link': 'blog-post/201708211647',
+        'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201708211647%2FTHUMBNAIL_AUGUST_21.jpg?alt=media&token=aedc69a2-14ed-4632-bf6a-fe05231fcc34',
+    },
+    {
         'title': '‫مدخل بيت مودرن!',
         'link': 'shopping',
         'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/shopping%2Fnine%2FTHUMBNAIL_AUGUST_16.jpg?alt=media&token=815452d8-46a9-4b8c-86b8-73bcc13bc57e',
@@ -9,15 +14,10 @@ const NewPosts = [
         'link': 'blog-post/201708141205',
         'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201708141205%2FTHUMBNAIL_AUGUST_14.jpg?alt=media&token=a2d96a0d-d4c9-4b33-9820-2ee94d4ed988',
     },
-    {
-        'title': 'حديقتي',
-        'link': 'blog-post/201708091825',
-        'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201708091825%2FTHUMBNAIL_AUGUST_9.jpg?alt=media&token=02d093ba-bc81-44d7-bd90-5c1b0948e6dd',
-    },
     // {
-    //     'title': 'أول مشاريعي',
-    //     'link': 'blog-post/201708071826',
-    //     'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201708071826%2FTHUMBNAIL_AUGUST_7.jpg?alt=media&token=a940e2e6-5001-4b06-99e6-e2e2295f1e63',
+    //     'title': 'حديقتي',
+    //     'link': 'blog-post/201708091825',
+    //     'imageURL': 'https://firebasestorage.googleapis.com/v0/b/era-farah-alhumaidhi.appspot.com/o/blog%2F201708091825%2FTHUMBNAIL_AUGUST_9.jpg?alt=media&token=02d093ba-bc81-44d7-bd90-5c1b0948e6dd',
     // },
 ];
 
